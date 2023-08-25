a009bd0b9503f074efbb79e2a327ad7888795297
gititub <61666085+gititub@users.noreply.github.com>
2023-08-25 11:28:16 +0200

    Update convert_pdf.py

--- convert_pdf.py
+++ convert_pdf.py
@@ -41,6 +41,23 @@ def convert_directory_to_text(input_directory, output_directory, max_length):
                 output_file.write(pdf_text)
             file_number += 1
 
+    file_list = os.listdir(output_directory)
+    num_subdirectories = len(file_list) // 120 + 1
+    for i in range(num_subdirectories):
+         subdirectory_name = f'subdirectory_{i+1}'
+         subdirectory_path = os.path.join(output_directory, subdirectory_name)
+         os.makedirs(subdirectory_path)
+         start_index = i * 120
+         end_index = min((i + 1) * 120, len(file_list))
+         files_to_move = file_list[start_index:end_index]
+         # Move the files to the subdirectory
+         for file_name in files_to_move:
+             source_path = os.path.join(output_directory, file_name)
+             destination_path = os.path.join(subdirectory_path, file_name)
+             shutil.move(source_path, destination_path)
+
+    print(f'Subdirectory "{subdirectory_name}" created with {len(files_to_move)} files.')
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("input_directory", help="Path to the input directory containing PDF files")
