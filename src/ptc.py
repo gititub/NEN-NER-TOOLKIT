b421022b8590e86d7d8a868a43a30dd9ceb42038
gititub <61666085+gititub@users.noreply.github.com>
2024-06-17 12:44:17 +0200

    Update ptc.py

--- src/ptc.py
+++ src/ptc.py
@@ -29,113 +29,128 @@ class Pubtator():
 
     @staticmethod
     def extract_pubtator(ids, output_format):
-        """Python function to extract full-text Pubtator results from a list of PMCIDs or PubMedIDs.
-        Returns results in biocjson format if output = "biocjson" and returns results
-        in pandas DataFrame format if output = "df".
-        """
-        print("Extracting PubTator results ...")
-        list_of_pubtators = []
-        error_sum = 0
-        id_sum = 0
-        all_relations_df_list = []
+    if not isinstance(ids, list):
+        id_list = [num.strip() for num in ids.split(',') if num.strip()]
+    else:
+        id_list = ids
+    print("Extracting PubTator results ...")
+    list_of_pubtators = []
+    error_sum = 0
+    id_sum = 0
+    all_relations_df_list = []
 
-        for id in ids:
-            print(f"Processing ID: {id}")
-            if id.startswith('PMC'):
-                url = f"https://www.ncbi.nlm.nih.gov/research/pubtator3-api/publications/pmc_export/biocjson?pmcids={id}"
-            else:
-                url = f"https://www.ncbi.nlm.nih.gov/research/pubtator3-api/publications/export/biocjson?pmids={id}&full=true"
-            try:
-                response = requests.get(url)
-                response.raise_for_status()
-                json_data = response.json()
-                if not json_data.get('passages'):
-                    print(f"No PubTator results found for ID: {id}")
-                    continue
-            except requests.exceptions.RequestException as e:
-                print(f"Error for {id}: {e}")
-                error_sum += 1
-            except ValueError as e:
-                print("Error parsing JSON data:", e)
-                print("URL:", url)
-                print("Response:", response.content)
-                error_sum += 1
-            else:
-                list_of_pubtators.append(json_data)
-                id_sum += 1
-                # Extract relations DataFrame and append to the list
-                relations_df = Pubtator.extract_relations(json_data, id)
-                all_relations_df_list.append(relations_df)
-
-        print(f"Total number of errors extracting full-text Pubtator data from PubMedIDs: {error_sum}")
-        print(f"Total number of articles annotated: {id_sum}")
-
-        df_list = []
-        for pubtator in list_of_pubtators:
-            data = []
-            if pubtator['_id'].split('|')[0] is not None:
-                PMID = pubtator['_id'].split('|')[0]
-            else:
-                PMID = pubtator['passages'][0]['infons'].get('article-id_pmid')
-
-            if pubtator['_id'].split('|')[1] is not None:
-                PMC = pubtator['_id'].split('|')[1]
-            else:
-                PMC = pubtator['passages'][0]['infons'].get('article-id_pmc')
-
-            for d in pubtator['passages']:
-                section_type = d['infons'].get('section_type')
-
-                for annotation in d['annotations']:
-                    entity_type = annotation['infons']['type']
-                    subtype = annotation['infons'].get('subtype')
-                    #tmVar = annotation['infons'].get('originalIdentifier')
-                    #identifiers = annotation['infons'].get('identifiers')
-                    offset = annotation['locations'][0]['offset']
-                    end = offset + annotation['locations'][0]['length']
-                    identifier = annotation['infons'].get('identifier')
-                    string_text = annotation['text']
-                    ncbi_homologene = annotation['infons'].get('ncbi_homologene')
-                    database = annotation['infons'].get('database')
-                    # Additional data for 'normalized' information
-                    normalized = annotation['infons'].get('normalized')
-                    normalized_id = annotation['infons'].get('normalized_id')
-                    biotype = annotation['infons'].get('biotype')
-                    name = annotation['infons'].get('name')
-                    accession = annotation['infons'].get('accession')
-
-                    data.append([
-                        PMID, PMC, section_type, string_text, offset, end, entity_type, subtype,
-                        identifier, ncbi_homologene, database, normalized,
-                        normalized_id, biotype, name, accession
-                    ])
-
-            df = pd.DataFrame(data,
-                              columns=['PMID', 'PMC', 'section_type', 'string_text', 'offset', 'end',
-                                       'entity_type', 'entity_subtype', 'identifier', 'ncbi_homologene',
-                                       'database', 'normalized', 'normalized_id', 'biotype', 'name', 'accession'])
-
-            df = df[df['identifier'].notna()]
-            df_list.append(df)
-
-        try:
-            all_relations_df = pd.concat(all_relations_df_list, ignore_index=True)
-            all_relations_df[['correlation_type', 'entity1', 'entity2']] = all_relations_df['type'].str.split('|', expand=True)
-            all_relations_df = all_relations_df.drop('type', axis=1)
-        except:
-            pass
-
-        if output_format == 'biocjson':
-            return list_of_pubtators, id_sum, error_sum
-        elif output_format == 'df':
-            if not df_list:
-                print("No articles with annotations found.")
-                return pd.DataFrame(), id_sum, error_sum
-            else:
-                merged_df = pd.concat(df_list, ignore_index=True)
-                return merged_df, all_relations_df, id_sum, error_sum
+    for id in id_list:
+        print(f"Processing ID: {id}")
+        if id.startswith('PMC'):
+            url = f"https://www.ncbi.nlm.nih.gov/research/pubtator3-api/publications/pmc_export/biocjson?pmcids={id}"
         else:
-            return None, None, id_sum, error_sum
+            url = f"https://www.ncbi.nlm.nih.gov/research/pubtator3-api/publications/export/biocjson?pmids={id}&full=true"
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            json_data = response.json()
+
+            # Extracting PubTator data from new JSON structure
+            for item in json_data["PubTator3"]:
+                passages = item.get("passages", [])
+                if passages:
+                    list_of_pubtators.append(item)
+                    id_sum += 1
+                    # Extract relations DataFrame and append to the list
+                    relations_df = extract_relations(item, id)
+                    all_relations_df_list.append(relations_df)
+
+        except requests.exceptions.RequestException as e:
+            print(f"Error for {id}: {e}")
+            error_sum += 1
+        except ValueError as e:
+            print("Error parsing JSON data:", e)
+            print("URL:", url)
+            print("Response:", response.content)
+            error_sum += 1
+
+    print(f"Total number of errors extracting Pubtator data: {error_sum}")
+    print(f"Total number of articles annotated: {id_sum}")
+
+    df_list = []
+    texts_list = []
+    for pubtator in list_of_pubtators:
+        data = []
+        full_text = []
+        if pubtator['_id'].split('|')[0] is not None:
+            PMID = pubtator['_id'].split('|')[0]
+        else:
+            PMID = pubtator['passages'][0]['infons'].get('article-id_pmid')
+
+        if pubtator['_id'].split('|')[1] is not None:
+            PMC = pubtator['_id'].split('|')[1]
+        else:
+            PMC = pubtator['passages'][0]['infons'].get('article-id_pmc')
+
+        for d in pubtator['passages']:
+            section_type = d['infons'].get('section_type')
+            text = d['text']
+            full_text.append({'PMID': PMID, 'PMC': PMC, 'Section': section_type, 'Text': text})
+            for annotation in d['annotations']:
+                entity_type = annotation['infons']['type']
+                subtype = annotation['infons'].get('subtype')
+                # tmVar = annotation['infons'].get('originalIdentifier')
+                # identifiers = annotation['infons'].get('identifiers')
+                offset = annotation['locations'][0]['offset']
+                end = offset + annotation['locations'][0]['length']
+                identifier = annotation['infons'].get('identifier')
+                string_text = annotation['text']
+                ncbi_homologene = annotation['infons'].get('ncbi_homologene')
+                database = annotation['infons'].get('database')
+                # Additional data for 'normalized' information
+                normalized = annotation['infons'].get('normalized')
+                normalized_id = annotation['infons'].get('normalized_id')
+                biotype = annotation['infons'].get('biotype')
+                name = annotation['infons'].get('name')
+                accession = annotation['infons'].get('accession')
+
+                data.append([
+                    PMID, PMC, section_type, string_text, offset, end, entity_type, subtype,
+                    identifier, ncbi_homologene, database, normalized,
+                    normalized_id, biotype, name, accession
+                ])
+
+        df = pd.DataFrame(data,
+                          columns=['PMID', 'PMC', 'section_type', 'string_text', 'offset', 'end',
+                                   'entity_type', 'entity_subtype', 'identifier', 'ncbi_homologene',
+                                   'database', 'normalized', 'normalized_id', 'biotype', 'name', 'accession'])
+
+        texts_list.extend(full_text)
+        df_full_text = pd.DataFrame(texts_list)
+        if not df_full_text.empty:
+            df_grouped = df_full_text.groupby(['PMID', 'Section'], sort=False).agg({'Text': ' '.join}).reset_index()
+            df_cleaned = df_grouped.dropna(axis=1, how='all')
+
+        df = df[df['identifier'].notna()]
+        df_list.append(df)
+
+    try:
+        all_relations_df = pd.concat(all_relations_df_list, ignore_index=True)
+        all_relations_df[['correlation_type', 'entity1', 'entity2']] = all_relations_df['type'].str.split('|',
+                                                                                                          expand=True)
+        all_relations_df = all_relations_df.drop('type', axis=1)
+    except:
+        pass
+
+    if output_format == 'biocjson':
+        if list_of_pubtators:
+            return json.dumps(list_of_pubtators, indent=4), json.dumps(list_of_pubtators, indent=4), json.dumps(
+                texts_list, indent=4)
+        else:
+            return f'No results found. Check if the PubMed or PMC ID is correct.'
+    elif output_format == 'df':
+        if not df_list:
+            print("No articles with annotations found.")
+            return pd.DataFrame(), pd.DataFrame(), pd.DataFrame()
+        else:
+            merged_df = pd.concat(df_list, ignore_index=True)
+            return merged_df, all_relations_df, df_cleaned
+
 
     @staticmethod
     def query(query, retmax, pub_date=None):
